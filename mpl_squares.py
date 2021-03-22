70aa2c271c78f0a15570f3240be867d66b0afb95
Punit-Choudhary <itspunitchoudhary@gmail.com>
2021-03-22 17:54:51 +0530

    a simple graph plotting

---
+++ mpl_squares.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+# We started with importing matplotlib's pyplot module as an alias plt.
+
+squares = [1, 4, 9, 16, 25] # List containing data
+plt.plot(squares) # plot() function plots a graph using passed data
+plt.show() # show() function renders graph
